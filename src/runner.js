3ae4fd757d4b4773ff3ad3a16b0ad35201fedfb9
lhuddlesto <lance299@gmail.com>
2019-10-08 16:58:27 -0400

    Removed comments

--- src/runner.js
+++ src/runner.js
@@ -6,18 +6,7 @@ const puppeteer = require('puppeteer');
     await page.setDefaultNavigationTimeout(20000); 
 
     await page.goto('https://soundcloud.com/nomad-fox')
-    const title = await page.title()
-    console.log(title)
-    // const data = await page.evaluate(() => {
-    //     const tracks = Array.from(document.querySelectorAll('.soundList__item'))
-    //     return tracks.map(td => td.textContent)
-    // })
-    // play.forEach(track => {
-    //     await track.click()
-    //     setTimeout(() => {
-    //        return
-    //     }, 15000)
-    // }) 
+    
     async function playTracks() {
         const tracks = await page.$$('.playButton')
 
